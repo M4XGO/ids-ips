0333b0805ec2a1bf47e9e8dfcf5d1c61dd993f2d
Maxime <maxgo.maxoue@gmail.com>
2025-01-14 21:03:06 +0100

    feat: tf state and security group to allow bastion connexion

---
+++ .cloud/terraform/security_groups.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Allow SSH access to bastion host"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Remplacez par votre adresse IP pour plus de sécurité
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion_sg"
+  }
+}
