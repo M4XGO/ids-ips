4b71094730c47511a341b11b92441599f1af9af6
Maxime <maxgo.maxoue@gmail.com>
2025-01-14 19:23:48 +0100

    feat: create role for ec2 and SSM communication

--- .cloud/terraform/iam.tf
+++ .cloud/terraform/iam.tf
@@ -17,3 +17,32 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
   role       = aws_iam_role.cloudwatch_agent_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+# Créer un rôle IAM pour EC2
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "EC2SSMRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attacher la policy SSM au rôle EC2
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Créer un profil d'instance IAM pour attacher le rôle aux EC2
+resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
+  name = "EC2SSMInstanceProfile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
